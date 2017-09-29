09f1727c140e96b7e69142f80e986f240fea0a31
Waris Agung Widodo <user@ido-alit.local>
2017-09-29 13:23:38 +0700

    Add: sidebar menu

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@
  * @Email:  ido.alit@gmail.com
  * @Filename: admin.js
  * @Last modified by:   user
- * @Last modified time: 2017-09-28T14:57:18+07:00
+ * @Last modified time: 2017-09-29T09:05:50+07:00
  */
 
 
@@ -15,7 +15,7 @@
  /* GET users listing. */
  router.get('/', function(req, res, next) {
    if (req.user) {
-     res.render('admin/index', {user: req.user})
+     return res.render('admin/index', {user: req.user})
    }
 
    res.redirect('/signin');
